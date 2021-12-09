737253cd70c5b2b40de8043ac3b3d81ae0fd3f35
Renukadeveloper96 <renukamaradagi@gmail.com>
2021-12-09 10:33:56 +0530

    loginregisterapi

---
+++ db.js
@@ -0,0 +1,7 @@
+var mongoose = require("mongoose");
+/*mongoose.connect(
+  "mongodb+srv://m001-student:vedantu123@sandbox.cou6h.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
+);*/
+mongoose.connect(
+  "mongodb+srv://zomato:zomato77@cluster0.nbutl.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
+)
