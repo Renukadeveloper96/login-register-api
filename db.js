6d199f2d4d5d78acbbe494603033635c1b3e537e
Renukadeveloper96 <renukamaradagi@gmail.com>
2022-04-14 14:19:50 +0530

    loginregisterapiz

--- db.js
+++ db.js
@@ -5,4 +5,5 @@ var mongoose = require("mongoose");
 /*mongoose.connect(
   "mongodb+srv://zomato:zomato77@cluster0.nbutl.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
 )*/
-mongoose.connect("mongodb+srv://zomato:zomato23@cluster0.nbutl.mongodb.net/loginregister?retryWrites=true&w=majority");
+//mongoose.connect("mongodb+srv://zomato:zomato23@cluster0.nbutl.mongodb.net/loginregister?retryWrites=true&w=majority");
+mongoose.connect("mongodb+srv://Renuka:renuka123@cluster0.nbutl.mongodb.net/loginregister?retryWrites=true&w=majority");
